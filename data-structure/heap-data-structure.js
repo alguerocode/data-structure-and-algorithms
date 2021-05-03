6f06d49fe693c3765a228ae785ef67f0a61db057
salah <alhashmis246@gmail.com>
2021-05-04 03:02:53 +0400

    'done'

--- data-structure/heap-data-structure.js
+++ data-structure/heap-data-structure.js
@@ -1,12 +1,12 @@
-// heap data structure
+// this.maxHeap data structure
 
 // left child = i * 2;
 // left child = i * 2 + 1;
 // parent = Math.floor(i / 2);
 
-class MinHeap {
+class maxHeap {
   constructor(size) {
-    this.maxHeap = [null];
+    this.maxthis.maxHeap = [null];
     this.size = size;
 
   }
@@ -14,13 +14,13 @@ class MinHeap {
     if (this.isFull()) {
       return;
     }
-    this.maxHeap.push(num);
-    if (this.maxHeap.length > 2) {
-      let idx = this.maxHeap.length - 1;
-      while (this.maxHeap[idx] > this.maxHeap[Math.floor(idx / 2)]) {
+    this.maxthis.maxHeap.push(num);
+    if (this.maxthis.maxHeap.length > 2) {
+      let idx = this.maxthis.maxHeap.length - 1;
+      while (this.maxthis.maxHeap[idx] > this.maxthis.maxHeap[Math.floor(idx / 2)]) {
         if (idx >= 1) {
-          this.maxHeap[idx] = this.maxHeap[Math.floor(idx / 2)];
-          this.maxHeap[Math.floor(idx / 2)] = this.maxHeap[idx];
+          this.maxthis.maxHeap[idx] = this.maxthis.maxHeap[Math.floor(idx / 2)];
+          this.maxthis.maxHeap[Math.floor(idx / 2)] = this.maxthis.maxHeap[idx];
           if (Math.floor(idx / 2) > 1) {
             idx = Math.floor(idx / 2);
           } else {
@@ -29,24 +29,40 @@ class MinHeap {
         }
       }
     }
-  }
+  };
   remove() {
-    if (this.isEmpty()) {
-      return;
-    }
-  }
-  isFull() {
-    if (this.maxHeap.length - 1 === this.size) {
-      console.log('the heap is full');
-      return true
-    }
-    return false
-  }
-  isEmpty() {
-    if (this.maxHeap.length <= 1) {
-      console.log('the heap is empty')
-      return true
-    }
-    return false
-  }
+		let smallest = this.maxHeaps[1];
+		if (this.maxHeap.length > 2) {
+			this.maxHeap[1] = this.maxHeap[this.maxHeap.length - 1];
+			this.maxHeap.splice(this.maxHeap.length - 1);
+			if (this.maxHeap.length == 3) {
+				if (this.maxHeap[1] < this.maxHeap[2]) {
+					[this.maxHeap[1], this.maxHeap[2]] = [this.maxHeap[2], this.maxHeap[1]];
+				};
+				return smallest;
+			};
+			let i = 1;
+			let left = 2 * i;
+			let right = 2 * i + 1;
+			while (this.maxHeap[i] <= this.maxHeap[left] || this.maxHeap[i] <= this.maxHeap[right]) {
+				if (this.maxHeap[left] > this.maxHeap[right]) {
+					[this.maxHeap[i], this.maxHeap[left]] = [this.maxHeap[left], this.maxHeap[i]];
+					i = 2 * i
+				} else {
+					[this.maxHeap[i], this.maxHeap[right]] = [this.maxHeap[right], this.maxHeap[i]];
+					i = 2 * i + 1;
+				};
+				left = 2 * i;
+				right = 2 * i + 1;
+				if (this.maxHeap[left] == undefined || this.maxHeap[right] == undefined) {
+					break;
+				};
+			};
+		} else if (this.maxHeap.length == 2) {
+			this.maxHeap.splice(1, 1);
+		} else {
+			return null;
+		};
+		return smallest;
+	};
 }
