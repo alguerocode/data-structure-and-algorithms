f9427c3706a4e2bc509ad039fe3fb7b4d504e0c7
salah alhashmi <alhashmis246@gmail.com>
2021-09-13 03:08:48 -0700

    add sort bubble

--- algorithms/sorting-and-searching/bubble-sort.js
+++ algorithms/sorting-and-searching/bubble-sort.js
@@ -1,1 +1,37 @@
-c
+// impelmenting bubble sort algorithms
+
+function bubbleSortByDescending(arr = []) {
+  if (!Array.isArray(arr)) throw new Error("argument must be array");
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (typeof arr[i] !== "number")
+        throw new Error("array must be array of numbers");
+      if (arr[i] < arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]; //swap operations
+      }
+    }
+  }
+  return arr;
+}
+function bubbleSortByAscending(arr) {
+  if (!Array.isArray(arr)) throw new Error("argument must be array");
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (typeof arr[i] !== "number")
+        throw new Error("array must be array of numbers");
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]; //swap operations
+      }
+    }
+  }
+  return arr;
+}
+
+// test both bubble sort
+
+const sortedArrByDescending = bubbleSortByDescending([3, 46, 4, 6, 1, 7, 5]);
+const sortedArrByAscending = bubbleSortByAscending([3, 46, 4, 6, 2, 7, 5]);
+console.log(sortedArrByDescending);
+console.log(sortedArrByAscending);
