8c30efb3b5a343f9ce6f2971c1dc9a282855a4b6
salah alhashmi <alhashmis246@gmail.com>
2021-09-14 05:56:19 -0700

    add quick-sort

--- algorithms/sorting-and-searching/quick-sort.js
+++ algorithms/sorting-and-searching/quick-sort.js
@@ -1,4 +1,26 @@
 // implementation of quick sort algorithm.
 
+function pivot(array, left = 0, right = array.length - 1) {
+  let shift = left;
+  for (let i = left + 1; i <= right; i++) {
+    if (array[i] < array[left]) {
+      shift++;
+      [array[i], array[shift]] = [array[shift], array[i]]; // swap operation
+    }
+  }
+  // finally swap the last;
+  [array[left], array[shift]] = [array[shift], array[left]];
+  return shift;
+}
 
-function quickSort()
+function quickSort(array, left = 0, right = array.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(array, left, right);
+    quickSort(array, left, pivotIndex - 1);
+    quickSort(array, pivotIndex + 1, right);
+  }
+  return array;
+}
+
+console.log(quickSort([9, 5, 1, 7, 3, 2, 6, 5, 8]));
+console.log(quickSort([9, 5, 1, 7, 3, 2, 6, 5, 8]));
